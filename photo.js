5318e0a560553e37af43fe6bce18b61b8c2812a5
Isaac Sunoo <isaacsunoo@gmail.com>
2019-01-06 21:56:16 -0700

    create photo js

--- photo.js
+++ photo.js
@@ -1,1 +1,22 @@
+class Photo {
+  constructor(id, title, caption, file, favorite) {
+    this.id = id;
+    this.title = title;
+    this.caption = caption;
+    this.file = file;
+    this.favorite = favorite || false;
+  }
 
+  saveToStorage() {
+    localStorage.setItem(this.id, JSON.stringify(this));
+  }
+
+  deleteFromStorage() {
+    localStorage.removeItem(this.id);
+  }
+
+  updatePhoto() {
+    
+  }
+
+}
